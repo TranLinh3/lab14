8b172ea9712a8acb922ff8266919e8cc4a4235e3
TranLinh3 <105622353+TranLinh3@users.noreply.github.com>
2022-06-24 11:23:59 +0700

    Update LyricWordUsingGeneric.java

--- Generic/LyricWordUsingGeneric.java
+++ Generic/LyricWordUsingGeneric.java
@@ -8,15 +8,12 @@ public class LyricWordUsingGeneric {
             new Word("best",4),new Word("when",5),new Word("you",6),
             new Word("say",7), new Word("nothing",8), new Word("at",9),
             new Word("all",10)};
-
     public static void main(String[] args) {
-        // Create a list that’s implemented by ArrayList
+      
         List<Word> lyric = new ArrayList<Word>();
-        // Add all String in string array to list
+  
         for (Word w : lyric)
             lyric.add(w);
-
-        //show all elements of words
         for (Word w : lyric) {
             System.out.print(w.getValue() + ", "+ w.getPosition());
         }
